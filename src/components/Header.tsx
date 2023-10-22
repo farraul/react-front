893a701fcc71d2470fedc6bd74011f0448cf01a5
raul far <raulfarllobell@gmail.com>
2023-10-22 22:13:57 +0200

    refactor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,7 +27,7 @@ function Header() {
   const [languages, setLanguages] = useState(i18n.services.resourceStore.data);
 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const handleMenu = (event: { currentTarget: React.SetStateAction<HTMLElement | null>; }) => {
+  const handleMenu = (event: { currentTarget: React.SetStateAction<HTMLElement | null> }) => {
     setAnchorEl(event.currentTarget);
   };
 
