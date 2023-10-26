5f3c3e379a705126639cab672a9d723675d736e3
raul far <raulfarllobell@gmail.com>
2023-10-26 18:56:05 +0200

    refactor

--- src/utilities/arrays.ts
+++ src/utilities/arrays.ts
@@ -1,28 +1,28 @@
 export const isEmpty = (value: number | null | string | undefined) => {
-    if (typeof value === 'number' && isNaN(value)) return true;
-    return value === undefined || value === null || value === '';
-  };
-  
-  export const AddQueryByField = (options: Record<string, any>) => {
-    const ArrayByField = Object.entries(options);
-  
-    let countQuery = 0;
-    const resultArray = ArrayByField.map(item => {
-      if (isEmpty(item[1])) return null;
-  
-      const query = `${item[0]}=${item[1]}`;
-  
-      if (!countQuery) {
-        countQuery = countQuery + 1;
-        return `?${query}`;
-      }
-  
+  if (typeof value === 'number' && isNaN(value)) return true;
+  return value === undefined || value === null || value === '';
+};
+
+export const AddQueryByField = (options: Record<string, any>) => {
+  const ArrayByField = Object.entries(options);
+
+  let countQuery = 0;
+  const resultArray = ArrayByField.map((item) => {
+    if (isEmpty(item[1])) return null;
+
+    const query = `${item[0]}=${item[1]}`;
+
+    if (!countQuery) {
       countQuery = countQuery + 1;
-      return `&${query}`;
-    });
-  
-    return resultArray
-      .filter(res => res !== null)
-      .join()
-      .replace(/,/g, '');
-  };
+      return `?${query}`;
+    }
+
+    countQuery = countQuery + 1;
+    return `&${query}`;
+  });
+
+  return resultArray
+    .filter((res) => res !== null)
+    .join()
+    .replace(/,/g, '');
+};
